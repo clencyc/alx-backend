24b949ec1df7a7142201f489bf1d8b52eadbfd74
clencyc <clency2023@gmail.com>
2024-08-21 23:55:03 +0300

    simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,9 +7,15 @@ range of indexes to return in a list
 for those particular pagination parameters.
 """
 
+
 def index_range(page: int, page_size: int):
     """
     function named index_range that takes two integer arguments page and page_size
     """
-    # return a tuple of size 2 containig a stat index and an end index
-    return tuple[2]
+    if page < 1 or page_size < 1:
+        raise  ValueError("Invalid page or per_page value.")
+    
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return start_index, end_index
