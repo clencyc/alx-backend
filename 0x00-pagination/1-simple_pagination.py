5fcf4855f1a0d9246c2b4d3521fc69dbf37ae890
clencyc <clency2023@gmail.com>
2024-08-22 16:36:40 +0300

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+"""
+Implement a method named get_page that takes
+two integer arguments page with default value
+1 and page_size with default value 10.
+"""
+
+
 import csv
 import math
 from typing import List
