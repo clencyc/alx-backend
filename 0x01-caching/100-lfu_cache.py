04502e23a049ed96e698acae88d6fb260d82b830
clencyc <clency2023@gmail.com>
2024-09-05 18:17:48 +0300

    finished

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,20 +1,41 @@
 #!/usr/bin/python3
 """
-LFU caching
+LFU caching module.
+This module contains the LFUCache class which implements
+a Least Frequently Used (LFU) caching system.
 """
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 class LFUCache(BaseCaching):
-    """ LFU cache class """
+    """ LFU cache class.
+    
+    Inherits from BaseCaching and implements a caching system
+    where the least frequently used items are discarded first when
+    the cache reaches its maximum capacity.
+    """
+    
     def __init__(self):
-        """ constructor """
+        """ Initialize the LFUCache.
+        
+        Initializes the cache data from the parent class and
+        additional structures to keep track of the order of keys
+        and their access frequencies.
+        """
         super().__init__()
         self.keys = []
         self.freq = {}
 
     def put(self, key, item):
-        """ method to store data in cache """
+        """ Add an item to the cache.
+        
+        If the cache is at its maximum capacity, it discards the
+        least frequently used item before adding the new item.
+        
+        Args:
+            key: The key under which the item is stored.
+            item: The item to store in the cache.
+        """
         if key is None or item is None:
             return
 
@@ -36,6 +57,17 @@ class LFUCache(BaseCaching):
         self.freq[key] = 1
 
     def get(self, key):
+        """ Retrieve an item from the cache.
+        
+        Increments the access frequency of the accessed item.
+        
+        Args:
+            key: The key of the item to retrieve.
+        
+        Returns:
+            The item associated with the key, or None if the key
+            is not in the cache.
+        """
         if key is None or key not in self.cache_data:
             return None
         self.keys.remove(key)
