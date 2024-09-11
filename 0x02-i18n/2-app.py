1e35e464c537bffbdf0ec90e15a6ad12dc1cca81
clencyc <clency2023@gmail.com>
2024-09-11 12:34:42 +0300

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -15,13 +15,14 @@ class config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
+
 
 @babel.localeselector
 def get_locale() -> str:
     """This function returns the locale from the request"""
     return request.accept_languages.best_match(Config['LANGUAGES'])
-
 
 @app.route('/', strict_slashes=False)
 def home():
@@ -29,6 +30,7 @@ def home():
     Returns the 2-index.html page
     """
     return render_template('2-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
