bb6251ed93c38214add946c1268ba092d80dfed5
clencyc <clency2023@gmail.com>
2024-09-11 12:08:20 +0300

    Basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,18 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 """
-Build flask app
+Build flask app that prints hello world
 """
-
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+
+
 def index():
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
