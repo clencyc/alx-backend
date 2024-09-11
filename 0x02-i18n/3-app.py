26c5bfe9806d9eefc732c8ac40bb8c0cc6a96f86
clencyc <clency2023@gmail.com>
2024-09-11 12:54:29 +0300

    parametrized templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -6,7 +6,8 @@ Demonstrates how to use Flask-Babel in a Flask app
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
+app = Flask(__name__)  # Create Flask app object
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 babel = Babel(app)
 
 
@@ -16,3 +17,7 @@ def home():
     Returns the 3-index.html page
     """
     return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
