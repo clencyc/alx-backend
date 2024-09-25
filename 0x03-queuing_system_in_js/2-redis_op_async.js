a32f5c5bbe14656e269ed96502aa94f0ecf91720
clencyc <clency2023@gmail.com>
2024-09-25 21:30:23 +0000

    Node Redis and async operations

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,33 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+    console.log('Redis client not connected to the server: ERROR_MESSAGE')
+})
+
+const getAsync = promisify(client.get).bind(client);
+
+function setNewSchool(schoolName,  value) {
+    client.set(schoolName, value, (err, reply) => {
+        console.log(`Reply: ${reply}`);
+    });
+}
+
+async function displaySchoolValue(schoolName) {
+    try {
+        const reply = await getAsync(schoolName);
+        console.log(reply);
+    } catch (err) {
+        console.log(`Error: ${err.message}`);
+    }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('Holberton', '100');
+displaySchoolValue('Holberton');
